02f33d470019b1fd1109b4c0c945265e34948164
Mroogal <pocztamacka1@gmail.com>
2020-05-04 21:42:16 +0200

    In file main.cpp change rysunek1 to rysunek3

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main(){
     Fish fish;
-    ofstream rysunek1("rysunek3.svg");
+    ofstream rysunek3("rysunek3.svg");
     //header(rysunek1);
     fish.draw(3,rysunek3);
     //closer(rysunek1);
